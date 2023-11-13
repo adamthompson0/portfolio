a2f221437934b7bf894f1a013fc3d8f9dc0eab6c
Adam Thompson <127151135+adamthompson0@users.noreply.github.com>
2023-11-13 11:52:30 -0800

    added files

---
+++ main.js
@@ -0,0 +1,86 @@
+const slider = document.querySelector('#slider');
+const prevButton = document.querySelector('#prev-button');
+const nextButton = document.querySelector('#next-button');
+const headerEl = document.querySelector('.header');
+
+
+function smoothScroll(event) {
+    event.preventDefault();
+    const targetId = event.currentTarget.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+    const headerHeight = 50; // Adjust this value based on your header height
+  
+    if (targetElement) {
+      const targetPosition = targetElement.getBoundingClientRect().top;
+      const startPosition = window.pageYOffset;
+      const distance = targetPosition - headerHeight;
+      const duration = 800; // Adjust the duration for the scrolling animation
+      let startTime = null;
+  
+      function scrollAnimation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const ease = Math.easeInOutQuad(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, ease);
+  
+        if (timeElapsed < duration) {
+          requestAnimationFrame(scrollAnimation);
+        }
+      }
+  
+      requestAnimationFrame(scrollAnimation);
+    }
+  }
+  
+  // Add click event listeners to navigation links
+  const navLinks = document.querySelectorAll('nav a');
+  navLinks.forEach(link => {
+    link.addEventListener('click', smoothScroll);
+  });
+  
+  // Easing function for smooth scrolling animation
+  Math.easeInOutQuad = function (t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+  };
+
+
+
+  let slideIndex = 0;
+
+  // Function to show the current slide
+  function showSlide() {
+    slider.style.transform = `translateX(-${slideIndex * 100}%)`;
+  }
+  
+  // Function to navigate to the previous slide
+  function prevSlide() {
+    if (slideIndex > 0) {
+      slideIndex--;
+      showSlide();
+    }
+  }
+  
+  // Function to navigate to the next slide
+  function nextSlide() {
+    if (slideIndex < slider.children.length - 1) {
+      slideIndex++;
+      showSlide();
+    }
+  }
+  
+// Add click event listeners to the navigation buttons
+prevButton.addEventListener('click', prevSlide);
+nextButton.addEventListener('click', nextSlide);
+
+  
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50){
+    headerEl.classList.add('header-scrolled');
+  } else if (window.scrollY <= 50){
+    headerEl.classList.remove('header-scrolled');
+  }
+});
